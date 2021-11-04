ebfd437df6ed4e9c813b78bbaf95a3be8e45d178
Ekmul <emil377g@edu.easj.dk>
2021-11-04 12:46:34 +0100

    Add project files.

---
+++ SensorRest/Controllers/SensorDataController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SensorDataLib;
+
+namespace SensorRest.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SensorDataController : ControllerBase
+    {
+        private static DateTime dt = DateTime.Now;
+        private static readonly List<SensorData> _data = new List<SensorData>
+        {
+            new SensorData("yeeeeeeeah", 80, dt.ToLongTimeString())
+        };
+        [HttpGet]
+        public List<SensorData> GetAllSensorData()
+        {
+            return new List<SensorData>(_data);
+        }
+        [HttpPost]
+        public SensorData Add(SensorData newData)
+        {
+            _data.Add(newData);
+            DateTime dt2 = DateTime.Now;
+            newData.TimeStamp = dt2.ToLongTimeString();
+            return newData;
+        }
+    }
+}
